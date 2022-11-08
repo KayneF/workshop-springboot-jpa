2333dbbada5c8d449f16ad6cf94f91dd3c9e2433
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-08 11:09:42 -0300

    OrderStatus enum

--- src/main/java/com/javacourse/course/entities/Order.java
+++ src/main/java/com/javacourse/course/entities/Order.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.javacourse.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -28,15 +29,17 @@ public class Order implements Serializable {
 			pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", 
 			timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
 	public Order() {}
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 	
@@ -52,6 +55,14 @@ public class Order implements Serializable {
 	}
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 	public User getClient() {
 		return client;
